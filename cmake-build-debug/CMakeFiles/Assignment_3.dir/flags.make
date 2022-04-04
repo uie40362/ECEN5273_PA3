475ce2680c56fa6eaae42d49ccb2bfb29c4df810
uie40362 <uie40362@contiwan.com>
2022-04-03 23:13:43 -0600

    Added Webpage caching

--- cmake-build-debug/CMakeFiles/Assignment_3.dir/flags.make
+++ cmake-build-debug/CMakeFiles/Assignment_3.dir/flags.make
@@ -6,5 +6,5 @@ C_FLAGS =  -pthread -g   -std=gnu11
 
 C_DEFINES = 
 
-C_INCLUDES = 
+C_INCLUDES = -I/usr/include/openssl 
 
--- httpechosrv.c
+++ httpechosrv.c
@@ -15,6 +15,9 @@
 #include <errno.h>
 #include <signal.h>
 #include <time.h>
+#include <openssl/md5.h>
+
+
 
 
 #define MAXLINE  8192  /* max text line length */
@@ -33,15 +36,26 @@ struct uri_info{
 struct ip_cache{
     char hostname[100];
     char ip[25];
+    pthread_rwlock_t rwlock;
     struct ip_cache *next;
+};
+
+struct web_cache{
+    char uri[120];
+    clock_t tick_start;
+    pthread_rwlock_t rwlock;
+    struct web_cache * next;
 };
 
 /*globals*/
 static volatile int keep_running = 1;
-pthread_rwlock_t cache_start_lock;
-//head ptr for ip cache
-struct ip_cache  *ipCache_start = NULL;
+pthread_rwlock_t ipcache_start_rwlock;
+pthread_rwlock_t webcache_start_rwlock;
+int timeout = 0;
 
+//head ptr for ip cache and web cache
+struct ip_cache  *ipCache_start = NULL;
+struct web_cache *webCache_start = NULL;
 
 
 /*function prototypes*/
@@ -54,19 +68,25 @@ int connect_via_name(char * hostname, int port);
 void parse_uri(char * uri, struct uri_info * server_info);
 void parse_hdr_info(char * hdr_line, char * data, int * host_provided);
 void addto_ipcache(char * hostname, char * ip);
+struct ip_cache * get_ipcache(char * hostname);
+void addto_webcache(char * uri, clock_t tickstart);
+struct web_cache * get_webcache(char * uri);
 
 int main(int argc, char **argv) 
 {
     setbuf(stdout, 0);
     int listenfd, *connfdp, port, clientlen=sizeof(struct sockaddr_in);
     struct sockaddr_in clientaddr;
-    pthread_t tid; 
+    pthread_t tid;
+    pthread_rwlock_init(&(webcache_start_rwlock), NULL);
+    pthread_rwlock_init(&(ipcache_start_rwlock), NULL);
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <port> <timeout>\n", argv[0]);
         exit(0);
     }
     port = atoi(argv[1]);
+    timeout = atoi(argv[2]);
 
     listenfd = open_listenfd(port);
     while (1) {
@@ -153,7 +173,13 @@ void service_http_request(int connfd){
     strcat(new_request, "\r\n");
 
     /*Connect to host server*/
-    int serv_sockfd = connect_via_name(serv_info.host, serv_info.port);
+    int serv_sockfd;
+    struct ip_cache * ptr = get_ipcache(serv_info.host);
+    if (ptr)
+        serv_sockfd = connect_via_ip(ptr->ip, serv_info.port);
+    else
+        serv_sockfd = connect_via_name(serv_info.host, serv_info.port);
+
     if (serv_sockfd<0){
         //handle for unsuccessful connection to server
         char httperr[50];
@@ -164,17 +190,61 @@ void service_http_request(int connfd){
         return;
     }
 
-    //send the modified http request to server
-    send(serv_sockfd, new_request, sizeof(new_request), 0);
+    MD5_CTX c;
+    unsigned char out[MD5_DIGEST_LENGTH];
 
-    //receive the reply
-    n = recv(serv_sockfd, response, sizeof(response), 0);
-    printf("sending the following response to client:\n");
-    while (n>0) {
-        printf("%s", response);
-        send(connfd, response, n, 0); //sending it to client web browser
-        bzero(response, sizeof(response));
+    MD5_Init(&c);
+    MD5_Update(&c, request_uri, strlen(request_uri));
+    MD5_Final(out, &c);
+    char md5string[33];
+    for(int i = 0; i < 16; ++i)
+        sprintf(&md5string[i*2], "%02x", (unsigned int)out[i]);
+
+    struct web_cache * webptr = get_webcache(request_uri);
+
+    if( webptr ) { //webpage in cache
+        int bytes_read;
+
+        //send cached webpage
+        /*open file and determine its size*/
+        FILE * fp = fopen(md5string, "r");
+        fseek(fp, 0, SEEK_END);
+        int size = ftell(fp);   //get size of file
+        fseek(fp, 0, SEEK_SET);
+        printf("sending the following CACHED response to client:\n");
+        while (size) {
+            bzero(response, sizeof(response));
+            bytes_read = fread(response, sizeof(char), sizeof(response), fp);
+
+            /*send buffer to server*/
+            send(connfd, response, bytes_read, 0);
+            printf("%s", response);
+            size -= bytes_read;
+        }
+    }
+
+    else { //webpage not in cache
+        //send the modified http request to server
+        send(serv_sockfd, new_request, sizeof(new_request), 0);
+
+        //receive the reply
         n = recv(serv_sockfd, response, sizeof(response), 0);
+        printf("sending the following response to client:\n");
+
+        //cache the webpage
+        FILE * fp = fopen(md5string, "w");
+//        printf("Value of errno: %d\n ", errno);
+
+        while (n > 0) {
+            printf("%s", response);
+            fwrite(response, sizeof(char), n, fp);
+            send(connfd, response, n, 0); //sending it to client web browser
+            bzero(response, sizeof(response));
+            n = recv(serv_sockfd, response, sizeof(response), 0);
+        }
+        fclose(fp);
+        clock_t start = clock();
+        addto_webcache(request_uri, start);
     }
 
 //    printf("received the following response from end server:\n%s", response);
@@ -234,11 +304,27 @@ void intHandler(int dummy) {
 
 /*IP caching function*/
 void addto_ipcache(char * hostname, char * ip){
-    struct ip_cache * pair;
+    pthread_rwlock_wrlock(&ipcache_start_rwlock);
+    struct ip_cache * pair = malloc(sizeof(struct ip_cache));
     strcpy(pair->hostname, hostname);
     strcpy(pair->ip, ip);
     pair->next = ipCache_start;
+    pthread_rwlock_init(&(pair->rwlock), NULL);
     ipCache_start = pair;
+    pthread_rwlock_unlock(&ipcache_start_rwlock);
+}
+
+struct ip_cache * get_ipcache(char * hostname){
+    struct ip_cache * ptr = ipCache_start;
+    while (ptr){
+        if(pthread_rwlock_trywrlock(&(ptr->rwlock)) == 0) {
+            if (strcmp(hostname, ptr->hostname) == 0)
+                return ptr;
+            pthread_rwlock_unlock(&(ptr->rwlock));
+        }
+        ptr = ptr->next;
+    }
+    return NULL;
 }
 
 /*connect to server via IP*/
@@ -246,9 +332,6 @@ int connect_via_ip(char * ip, int port){
     if (!port)
         port = 80;
 
-    int ip_valid;
-    struct in_addr ipaddr;
-    struct hostent * server;
     struct sockaddr_in serveraddr;
 
     /* socket: create the socket */
@@ -258,26 +341,12 @@ int connect_via_ip(char * ip, int port){
         return -1;
     }
 
-    ip_valid = inet_aton(ip, &ipaddr);
-    if (!ip_valid){
-        printf("IP not valid");
-        //handle for bad IPs
-        return -1;
-    }
-
-    /* gethostbyaddr: get the server based on IP Address*/
-    server = gethostbyaddr((const void *)&ipaddr, sizeof(ipaddr), AF_INET);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host as %s\n", ip);
-        //handle for bad IPs
-        return -1;
-    }
 
     /* build the server's Internet address */
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-          (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    bcopy(ip,
+          (char *)&serveraddr.sin_addr.s_addr, sizeof(ip));
     serveraddr.sin_port = htons(port);
 
     //connect to host server
@@ -382,5 +451,33 @@ void parse_hdr_info(char * hdr_line, char * data, int * host_provided){
     }
 
     sprintf(data, "%s%s\r\n", data, hdr_line);
+}
+
+/*adds uri to linked list*/
+void addto_webcache(char * uri, clock_t tickstart){
+    pthread_rwlock_wrlock(&webcache_start_rwlock);
+    struct web_cache * pair = malloc(sizeof(struct web_cache));
+    strcpy(pair->uri, uri);
+    pair->tick_start = tickstart;
+    pair->next = webCache_start;
+    pthread_rwlock_init(&(pair->rwlock), NULL);
+    webCache_start = pair;
+    pthread_rwlock_unlock(&webcache_start_rwlock);
+}
+
+struct web_cache * get_webcache(char * uri){
+    struct web_cache * ptr = webCache_start;
+    clock_t diff;
+    while (ptr){
+        if(pthread_rwlock_trywrlock(&(ptr->rwlock)) == 0) {
+            diff = clock() - ptr->tick_start;
+            diff = diff/CLOCKS_PER_SEC;
+            if (strcmp(uri, ptr->uri) == 0 && diff<timeout)
+                return ptr;
+            pthread_rwlock_unlock(&(ptr->rwlock));
+        }
+        ptr = ptr->next;
+    }
+    return NULL;
 }
 
